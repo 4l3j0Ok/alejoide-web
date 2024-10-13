95d7164778a7bb6a20bcce22147c6081f5e6ea1d
4l3j0Ok <alejofsarmiento@gmail.com>
2024-10-13 02:56:12 -0300

    feat: add projects section

---
+++ src/config/projects.js
@@ -0,0 +1,28 @@
+import myLinksImage from "../images/projects/my_links.webp";
+import bcraScraperApiImage from "../images/projects/bcra_scraper_api.webp";
+import prodeskStacksImage from "../images/projects/prodesk_stacks.webp";
+
+
+export const projects = [
+  {
+    title: "My Links",
+    description: "Mi web de links profesionales y personales",
+    image: myLinksImage,
+    webUrl: "https://links.alejoide.com",
+    repoUrl: "https://github.com/4l3j0Ok/my-links-web",
+  },
+  {
+    title: "BCRA Scraper API",
+    description: "Una API de código abierto que permite obtener los bancos registrados en el BCRA.",
+    image: bcraScraperApiImage,
+    webUrl: "https://bcra-scraper-api.alejoide.com",
+    repoUrl: "https://github.com/4l3j0Ok/my-links-web",
+  },
+  {
+    title: "Prodesk Stacks",
+    description: "Echale un vistazo a los stacks que tengo levantados en mi propio servidor de casa.",
+    image: prodeskStacksImage,
+    webUrl: null,
+    repoUrl: "https://github.com/4l3j0Ok/prodesk-stacks",
+  },
+]
