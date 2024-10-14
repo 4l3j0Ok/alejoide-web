dbb72be78467e9a2ac1f2ae5cd6979be66549649
4l3j0Ok <alejofsarmiento@gmail.com>
2024-10-14 17:23:15 -0300

    feat: Add integration for astro-icon and add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://alejoide.com",
+  integrations: [icon()],
 });
