56ce23a953b67811da86e1ad01063bc9a210820b
4l3j0Ok <alejofsarmiento@gmail.com>
2024-10-15 00:51:35 -0300

    feat: Add astro config for environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import icon from "astro-icon";
 
@@ -7,4 +7,11 @@ import icon from "astro-icon";
 export default defineConfig({
   site: "https://alejoide.com",
   integrations: [icon()],
+  env: {
+    schema: {
+      SEND_EMAIL_FROM: envField.string({ context: "server", access: "secret" }),
+      RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
+      GOOGLE_ANALYTICS_TAG: envField.string({ context: "client", access: "public" }),
+    }
+  }
 });
