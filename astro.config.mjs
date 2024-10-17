803901defaee0f5bb62b8c25bb5c48f442ae2021
4l3j0Ok <alejofsarmiento@gmail.com>
2024-10-16 23:47:44 -0300

    feat: add node and react integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig, envField } from 'astro/config';
 
 import icon from "astro-icon";
 
+import node from "@astrojs/node";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://alejoide.com",
-  integrations: [icon()],
+  integrations: [icon(), react()],
+
   env: {
     schema: {
       SEND_EMAIL_FROM: envField.string({ context: "server", access: "secret" }),
@@ -14,5 +19,11 @@ export default defineConfig({
       RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
       GOOGLE_ANALYTICS_TAG: envField.string({ context: "client", access: "public" }),
     }
-  }
+  },
+
+  output: "server",
+
+  adapter: node({
+    mode: "standalone"
+  })
 });
