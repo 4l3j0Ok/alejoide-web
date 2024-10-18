fa9541d76199c1d5bee60bdf79326588430f11e2
4l3j0Ok <alejofsarmiento@gmail.com>
2024-10-17 22:52:02 -0300

    feat: Add logo and social links to footer

---
+++ src/config/footer.ts
@@ -0,0 +1,6 @@
+import { PUBLIC_EMAIL } from "astro:env/client";
+
+
+export const githubUrl = "https://github.com/4l3j0Ok";
+export const linkedinUrl = "https://www.linkedin.com/in/alejoide";
+export const email = PUBLIC_EMAIL;
